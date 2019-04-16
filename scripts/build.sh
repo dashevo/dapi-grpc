04b6dc1be68c2400e59cf9a0a6bd611fae37aa24
Ivan Shumkov <ivan@shumkov.ru>
2019-04-16 14:54:24 +0300

    Add TransactionsFilterStream definition and build config

---
+++ scripts/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Build "grpcweb/common" until it is updated regularly on Docker Hub
+docker build -t grpcweb/common .
+
+# Generate GRPC-Web client for DapiTransactionsFilterStream service
+OUT_PATH="$PWD/dist/web"
+
+mkdir -p dist/web
+
+docker run -v "$PWD:$PWD" \
+           grpcweb/common \
+           protoc -I="$PWD" "tx_filter_stream.proto" \
+                   --js_out="import_style=commonjs:$OUT_PATH" \
+                   --grpc-web_out="import_style=commonjs,mode=grpcwebtext:$OUT_PATH"
