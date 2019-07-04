4e8acfdda3ec4e2f5b926fb3f27ad1acc08f3bc4
Djavid Gabibiyan <djavidoff@gmail.com>
2019-07-04 14:06:07 +0500

    Export server interceptor and converters

--- node.js
+++ node.js
@@ -4,8 +4,22 @@ const coreMessages = require('./clients/nodejs/core_pb');
 const transactionsFilterStreamMessages = require('./clients/nodejs/transactions_filter_stream_pb');
 const loadPackageDefinition = require('./src/loadPackageDefinition');
 
+const jsonToProtobufFactory = require('./src/converters/jsonToProtobufFactory');
+const protobufToJson = require('./src/converters/protobufToJson');
+
+const conversionInterceptorFactory = require('./src/interceptors/server/conversionInterceptorFactory');
+
 module.exports = Object.assign({
   CorePromiseClient,
   TransactionsFilterStreamPromiseClient,
   loadPackageDefinition,
+  converters: {
+    jsonToProtobufFactory,
+    protobufToJson,
+  },
+  interceptors: {
+    server: {
+      conversionInterceptorFactory,
+    },
+  },
 }, coreMessages, transactionsFilterStreamMessages);
